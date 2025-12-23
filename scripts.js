ab8694e0e34bddf7307315b1b6ecf8daa01edf21
joyanta debnath <joydeb050@gmail.com>
2025-12-23 07:38:51 -0600

    added initial files

---
+++ scripts.js
@@ -0,0 +1,26 @@
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('main section');
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < triggerBottom) {
+            section.classList.add('show');
+        } else {
+            section.classList.remove('show');
+        }
+    });
+});
+
+// Smooth scrolling
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
